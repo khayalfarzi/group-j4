dee678b58e2315c6ae1d0a7e050e8d203f836960
r.askerov <defero.askerov@gmail.com>
2021-12-07 20:49:44 +0400

    problem fixed

--- src/main/java/az/iktlab/group_j4/java_lessons/lesson_6/StringArrEx.java
+++ src/main/java/az/iktlab/group_j4/java_lessons/lesson_6/StringArrEx.java
@@ -1,15 +1,33 @@
 package az.iktlab.group_j4.java_lessons.lesson_6;
 
 import java.util.Arrays;
+import java.util.Scanner;
+
+//public class StringArrEx {
+//
+//    public static void main(String[] args) {
+//        String[] arr = new String[2];
+//
+//        arr[0] = "Abnzvfgjsv";
+//        arr[1] = "jshdvfchj";
+//
+//        System.out.println(arr.length);
+//    }
+//}
 
 public class StringArrEx {
 
     public static void main(String[] args) {
-        String[] arr = new String[2];
-
-        arr[0] = "Abnzvfgjsv";
-        arr[1] = "jshdvfchj";
-
-        System.out.println(arr.length);
+//        Scanner scan = new Scanner(System.in);
+//        int i = scan.nextInt();
+//
+//        double d = scan.nextDouble();
+//
+//        String s = ((String) i);
+//        //scan.close();
+//
+//        System.out.println("String: " + s);
+//        System.out.println("Double: " + d);
+//        System.out.println("Int: " + i);
     }
 }
