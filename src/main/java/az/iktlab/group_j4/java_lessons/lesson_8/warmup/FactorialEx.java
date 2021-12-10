c723ccb16382f9fca59f994024acbae90694141e
nicatguliyev <nicatguliyev3@gmail.com>
2021-12-10 08:40:43 -0800

    Calculated Factorial

--- src/main/java/az/iktlab/group_j4/java_lessons/lesson_8/warmup/FactorialEx.java
+++ src/main/java/az/iktlab/group_j4/java_lessons/lesson_8/warmup/FactorialEx.java
@@ -3,6 +3,11 @@ package az.iktlab.group_j4.java_lessons.lesson_8.warmup;
 public class FactorialEx {
 
     public static int calcFactorial(int num) {
-        return 0;
+        if(num == 1){
+            return 1;
+        }
+        else{
+            return num * calcFactorial(num - 1);
+        }
     }
 }
