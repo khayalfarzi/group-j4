29ac853f4803bd6f12222d5508a6fa76bb6e8ac7
pervin <pervin.yunusov.1996@gmail.com>
2021-12-10 20:51:51 +0400

    FIRST

--- src/main/java/az/iktlab/group_j4/java_lessons/lesson_8/warmup/FactorialEx.java
+++ src/main/java/az/iktlab/group_j4/java_lessons/lesson_8/warmup/FactorialEx.java
@@ -1,13 +1,30 @@
 package az.iktlab.group_j4.java_lessons.lesson_8.warmup;
 
+import java.util.Scanner;
+
 public class FactorialEx {
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println(calcFactorial(n));
+
+
+    }
+
+
+
     public static int calcFactorial(int num) {
-        if(num == 1){
+        if(num==0){
             return 1;
+        }else{
+            return num * calcFactorial(num-1);
         }
-        else{
-            return num * calcFactorial(num - 1);
-        }
+
+
+
+
     }
 }
