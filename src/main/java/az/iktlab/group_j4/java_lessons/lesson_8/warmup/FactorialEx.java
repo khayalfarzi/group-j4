976d7fae370706646ab92eafe40fc0cd8dbd4d6f
Khayal Farziyev <khayalfarzi@gmail.com>
2021-12-10 08:58:56 -0800

    lesson 8: warmup oop

--- src/main/java/az/iktlab/group_j4/java_lessons/lesson_8/warmup/FactorialEx.java
+++ src/main/java/az/iktlab/group_j4/java_lessons/lesson_8/warmup/FactorialEx.java
@@ -3,6 +3,6 @@ package az.iktlab.group_j4.java_lessons.lesson_8.warmup;
 public class FactorialEx {
 
     public static int calcFactorial(int num) {
-        return 0;
+        return num == 1 ? 1 : calcFactorial(num - 1) * num;
     }
 }
